4d958ff074d6fdb3317b630514a78ab435715ef0
Oleg Bugaenko <bugaenko.kbu@gmail.com>
2022-10-29 11:28:51 +0300

    Create class Employee

---
+++ src/main/java/Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+    //employee data
+    private String nameAndLastName;
+    private int age;
+    private String email;
+    private String employment;
+    private String telephoneNumber;
+
+    public Employee(String nameAndLastName, int age, String email, String employment, String telephoneNumber) {
+        this.nameAndLastName = nameAndLastName;
+        this.age = age;
+        this.email = email;
+        this.employment = employment;
+        this.telephoneNumber = telephoneNumber;
+    }
+
+}
